52f870eb3e3b2fad3ad37dcc1dc4dc9582cd6a3f
kniroj-neupane <knirojneupane@gmail.com>
2025-02-09 22:21:00 +0545

    Added geohashing

--- app/Models/Stop.php
+++ app/Models/Stop.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\GeoHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,7 +17,8 @@ class Stop extends Model
     protected $fillable = [
         'name',
         'location_lat',
-        'location_lng'
+        'location_lng',
+        'geohash'
     ];
     /**
      * The routes that the stop belongs to.
@@ -25,5 +27,16 @@ class Stop extends Model
     {
         return $this->belongsToMany(Route::class, 'route_stop')
             ->withPivot('order'); //withPivot retrives the order column from the pivot table 'route_stop'
+    }
+
+    /**
+     * Boot method to generate geohash before saving.
+     */
+    protected static function booted()
+    {
+        static::saving(function ($stop) {
+            // Automatically generate and set the geohash before saving the Stop
+            $stop->geohash = GeoHelper::encodeGeohash($stop->location_lat, $stop->location_lng, 7); // You can adjust precision as needed
+        });
     }
 }
