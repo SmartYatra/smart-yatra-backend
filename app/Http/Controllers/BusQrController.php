f6b982d4ce740e4781f17c2dfa45c9a79b50b499
kniroj-neupane <knirojneupane@gmail.com>
2025-02-28 23:08:29 +0545

    Remove bus id from param

--- app/Http/Controllers/BusQrController.php
+++ app/Http/Controllers/BusQrController.php
@@ -5,22 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\API\BaseController;
 use App\Models\Bus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class BusQrController extends BaseController
 {
-    public function getQrData($busId)
+    public function getQrData()
     {
+        $driver = Auth::user();
+        if (!$driver || $driver->type != 'driver')
+            return $this->sendError("User Not found or user is not a driver.", [], 404);
         // Fetch the bus record
-        $bus = Bus::find($busId);
-
-        // Check if the bus exists
-        if (!$bus) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Bus not found.'
-            ], 404);
-        }
+        $bus = $driver->hasBus;
+        if (!$bus)
+            return $this->sendError("No bus assigned to the driver", [], 404);
 
         // Generate QR data
         $qrData = [
